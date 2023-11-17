8ec7ae8e7a75d115c5ed404adb2287870f211e7c
Ramkumar <87489042+Ramkumar-Programer@users.noreply.github.com>
2023-11-17 14:25:43 +0530

    Update Register.js

--- Router/Athu/Register.js
+++ Router/Athu/Register.js
@@ -5,14 +5,16 @@ const router = express.Router();
 const dbConfig = require('../../Config/Config');
 const {insertNewUserQuery,createListTableQuery,createCardTableQuery,executeQuery, selectUserQuery} = require('../../Config/Queries')
 
-
 router.post('/addUser', async  (req, res) =>{
     
+    console.log(req.body)
     try{
+        const password = hashPassword(req.body.password);
+
         const tableListName = req.body.email_id.replace('@', '_').replace('.', '_') + '_list';
         const tableCardName = req.body.email_id.replace('@', '_').replace('.', '_') + '_card';
 
-        await executeQuery(dbConfig, insertNewUserQuery, [req.body.first_name, req.body.last_name, req.body.email_id, req.body.password]);
+        await executeQuery(dbConfig, insertNewUserQuery, [req.body.first_name, req.body.last_name, req.body.email_id, password]);
         await executeQuery(dbConfig, createListTableQuery(tableListName));
         await executeQuery(dbConfig, createCardTableQuery(tableCardName, tableListName));
 
@@ -43,4 +45,38 @@ router.post('/emailIdExists', async (req, res) => {
     }
   });
 
+router.post('/login', async (req, res) => {
+    try {
+      const result = await executeQuery(dbConfig, selectUserQuery, [req.body.email_id]);
+      const password = await hashPassword(req.body.password);
+      console.log(password)
+      if (result.length > 0) {
+      
+        if(result[0].password === password)
+        {
+          res.status(200).json({ status: true, message: 'Sucesss', name : result[0].firstName+result[0].lastName });
+        }
+        else
+        {
+          res.status(200).json({ status: false, message: 'Incorrect Password' });
+        }
+        
+      } else {
+        // Email ID does not exist
+        res.status(200).json({ status: false, message: 'Email Id is not exists' });
+      }
+    } catch (error) {
+      console.error("EmailId Exists error:", error);
+      res.status(500).json("EmailId exists Api is failed");
+    }
+  });
+
+
+function hashPassword(password) {
+    const saltRounds = 7; // Adjust as needed
+    const salt = crypto.randomBytes(16).toString('hex'); // CSPRNG for salt
+    const pepperedPassword = password + process.env.SECRET_KEY; // Combine password with secret key
+    return bcrypt.hashSync(pepperedPassword, bcrypt.genSaltSync(saltRounds));
+  
+}
 module.exports = router;
