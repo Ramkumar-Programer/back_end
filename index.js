7176bc5d1317dbc4218eb9ea78b911d35fb6f884
Ramkumar <87489042+Ramkumar-Programer@users.noreply.github.com>
2023-11-17 12:56:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,11 @@ const registerRouter = require('./Router/Athu/Register')
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+//app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:3000',
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+}));
 
 
 connectAndInitializeDatabase();
