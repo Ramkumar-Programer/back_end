4483788c3c6e03ade3c9d5049a473b9ed1f49f88
Ramkumar <87489042+Ramkumar-Programer@users.noreply.github.com>
2023-11-17 14:16:56 +0530

    Update Config.js

--- Config/Config.js
+++ Config/Config.js
@@ -2,12 +2,21 @@
 
 const mySql = require('mysql2');
 
+// const dbConfig = mySql.createConnection({
+//     host : "beh45le33j6hw6iwj8r6-mysql.services.clever-cloud.com",
+//     port : 3306,
+//     user : "uukefphjyhdqc756",
+//     password : "tU2PbCGzJPupbBkxDok4",
+//     database : "beh45le33j6hw6iwj8r6"
+// });
+
 const dbConfig = mySql.createConnection({
-    host : "beh45le33j6hw6iwj8r6-mysql.services.clever-cloud.com",
-    port : 3306,
-    user : "uukefphjyhdqc756",
-    password : "tU2PbCGzJPupbBkxDok4",
-    database : "beh45le33j6hw6iwj8r6"
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE
 });
+
 
 module.exports = dbConfig;
