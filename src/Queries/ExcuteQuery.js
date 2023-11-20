8191ca422a08939e1463b7128e32dbf822598201
Ramkumar <87489042+Ramkumar-Programer@users.noreply.github.com>
2023-11-20 12:52:43 +0530

    Update ExcuteQuery.js

--- src/Queries/ExcuteQuery.js
+++ src/Queries/ExcuteQuery.js
@@ -1,26 +1,26 @@
 
 
-// async function executeQuery(db, query, params = []) {
-  
-//     return new Promise((resolve, reject) => {
-//         db.query(query, params, (err, result) => {
-//             if (err) {
-//                 reject(err);
-//             } else {
-//                 resolve(result);
-//             }
-//         });
-//     });
-// }
-
 async function executeQuery(db, query, params = []) {
-  try {
-    const result = await db.promise().query(query, params);
-    return result[0]; // Assuming you want to return the result data
-  } catch (error) {
-    throw error;
-  }
+  
+    return new Promise((resolve, reject) => {
+        db.query(query, params, (err, result) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(result);
+            }
+        });
+    });
 }
+
+// async function executeQuery(db, query, params = []) {
+//   try {
+//     const result = await db.promise().query(query, params);
+//     return result[0]; // Assuming you want to return the result data
+//   } catch (error) {
+//     throw error;
+//   }
+// }
 
 
 
