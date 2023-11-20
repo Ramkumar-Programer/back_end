621e243f3bfaaaaa49b01c515b333b67a09159a7
Ramkumar <87489042+Ramkumar-Programer@users.noreply.github.com>
2023-11-20 11:12:25 +0530

    Update Config.js

--- src/Config/Config.js
+++ src/Config/Config.js
@@ -3,11 +3,11 @@
 const mySql = require('mysql2');
 
 const dbConfig = mySql.createConnection({
-    host : "localhost",
-    port : 3306,
-    user : "root",
-    password : "root",
-    database : "cardsboard"
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE
 });
 
 module.exports = dbConfig;
